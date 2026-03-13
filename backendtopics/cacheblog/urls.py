c17e19302785496b24f2f4b7f654e4839325e8d9
Abdulkhafid001 <kabiruabdulkhafid@gmail.com>
2026-03-13 23:03:05 +0100

    add pagination feature

--- backendtopics/cacheblog/urls.py
+++ backendtopics/cacheblog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('blog/', views.blog),
+    path('blog/', views.paginated_blogposts),
     path('blog/detail/<int:pk>', views.BlogDetailView.as_view())
 ]
--- backendtopics/cacheblog/views.py
+++ backendtopics/cacheblog/views.py
@@ -3,6 +3,7 @@ from django.views.generic import DetailView
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from django.core.cache import cache
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Blog
 
 # Create your views here.
@@ -28,3 +29,16 @@ def get_recent_blogposts():
         posts = Blog.objects.all()
         cache.set('recent-posts', posts, 60 * 5)  # cache for 10 minutes
     return posts
+
+
+def paginated_blogposts(request):
+    posts_list = Blog.objects.all()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(posts_list, 3)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog.html', {'posts': posts})
